bda2f0c1a1a44fce8b1e4a70ac0b05ddc00910ce
daryldixon1994 <kmohamedanouar@gmail.com>
2024-05-27 22:37:26 +0100

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+// write your middleware here
+
+
+
+
+
+// home page route here => path : /
+
+
+
+
+
+// services page route here => path : /services
+
+
+
+
+
+// contact page route here => path : /contact
+
+
+
+// listen to your application here
